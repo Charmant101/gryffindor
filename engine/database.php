6654fe88244de7ac512354b190ec646bfc24f284
Oladele Omoarukhe <daylayomoarukhe@yahoo.com>
2019-09-20 21:30:58 +0100

    authentication

--- engine/database.php
+++ engine/database.php
@@ -4,6 +4,7 @@ define('username', 'yywbaokahafldj');
 define('password', 'b2f7d6723b6040aa120f12a2da3fb31f2563db54afc0dfe2034265ca0dcb8fb5');
 define('database', 'dahppq2r9t9fei');
 
+
 $mycon= mysqli_connect(server, username, password, database);
 
 
