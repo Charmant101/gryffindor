84dbe016064cfceffb59bc8316d4db3a6715d027
Oladele Omoarukhe <daylayomoarukhe@yahoo.com>
2019-09-20 22:34:40 +0100

    changes to database

--- engine/database.php
+++ engine/database.php
@@ -1,11 +1,11 @@
 <?php 
-define('server', 'ec2-184-73-169-163.compute-1.amazonaws.com');
-define('username', 'yywbaokahafldj');
-define('password', 'b2f7d6723b6040aa120f12a2da3fb31f2563db54afc0dfe2034265ca0dcb8fb5');
-define('database', 'dahppq2r9t9fei');
+define('Host', 'ec2-174-129-227-51.compute-1.amazonaws.com');
+define('Database', 'd832cabcms4fa7');
+define('User', 'xzzrklunobjynv');
+define('Port', '5432');
+define('Password','0af130b9f60b2048f273cf43e6577f373af06b7c5a518fd5a1a5041a1605e6cc');
 
-
-$mycon= mysqli_connect(server, username, password, database);
+$mycon= mysqli_connect(Host, Database, Port,Password);
 
 
 
